2cfd0570eaa2926fcc5e918c2ef3ceba2111d0a1
Ahim11 <ahim.mudbari@gmail.com>
2022-08-04 20:23:27 +0545

    root of the project

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-const Joi = require('joi');
 const genres = require('./routes/genres');
 const home = require('./routes/home');
-const mogoose = require('mongoose');
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose');
+const customer = require('./routes/customers');
 
 require('dotenv').config();
 const port = process.env.PORT || 3500;
@@ -20,37 +19,8 @@ mongoose
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/', home);
+app.use('/api/customers', customer);
 
-const movieSchema = new mongoose.Schema({
-  name: { type: String },
-  genre: String,
-  date: { type: Date, default: Date.now },
-  quality: [String],
-  isReleased: Boolean,
-});
-
-const Movies = mongoose.model('Movies', movieSchema);
-
-// async function createMovie() {
-//   const movie = new Movies({
-//     name: 'Alpha',
-//     genre: 'Comedy',
-//     quality: ['720p', '1080p'],
-//     isReleased: true,
-//   });
-//   const result = await movie.save();
-//   console.log(result);
-// }
-// createMovie();
-
-async function getMovie() {
-  const movie = await Movies.find({ isReleased: true })
-    .sort({ name: 1 })
-    .limit(3)
-    .select(['name', 'genre']);
-  console.log(movie);
-}
-getMovie();
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
 });
