d5bf5b48e7213548e6c16249b5bc6ddd562b3911
Ahim11 <ahim.mudbari@gmail.com>
2022-08-06 22:00:40 +0545

    schema and model for rental with joi validation and custom hybrid approach to create collection of object

---
+++ models/rental.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 100,
+      },
+      isGold: {
+        type: Boolean,
+        required: false,
+      },
+      phone: {
+        type: Number,
+        required: true,
+        min: 9800000000,
+        max: 9899999999,
+      },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        minlength: 2,
+        trim: true,
+        maxlength: 100,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100,
+      },
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  dateReturned: {
+    type: Date, //set on server
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+    max: 10000000, //set on server
+  },
+});
+const Rental = mongoose.model('Rental', rentalSchema);
+
+function validateRental(rental) {
+  const schema = Joi.object({
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required(),
+  });
+  return schema.validate(rental);
+}
+
+module.exports.Rental = Rental;
+module.exports.validateRental = validateRental;
