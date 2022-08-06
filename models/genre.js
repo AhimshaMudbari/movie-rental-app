282e68cce0846c9cb58eccab6d19dbe6f247d07b
Ahim11 <ahim.mudbari@gmail.com>
2022-08-06 21:58:27 +0545

    schema and model for genre with joi validation

--- models/genre.js
+++ models/genre.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const Genre = mongoose.model(
-  'Genre',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 3,
-      maxlength: 55,
-      lowercase: true,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 55,
+    lowercase: true,
+  },
+});
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
   const schema = Joi.object({
@@ -21,5 +19,6 @@ function validateGenre(genre) {
   return schema.validate(genre);
 }
 
+module.exports.genreSchema = genreSchema;
 module.exports.Genre = Genre;
 module.exports.validateGenre = validateGenre;
