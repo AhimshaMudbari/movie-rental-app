6e807ccdde45b92b223980d117a5ec17a2c9e5fc
Ahim11 <ahim.mudbari@gmail.com>
2022-08-06 21:59:27 +0545

    schema and model for movie with joi validation

---
+++ models/movie.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const { genreSchema } = require('./genre');
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 100,
+  },
+  genre: {
+    type: genreSchema,
+    required: true,
+  },
+  numberInStock: {
+    type: Number,
+    min: 0,
+    max: 100,
+  },
+  dailyRentalRate: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+  },
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+function validateMovie(movie) {
+  const schema = Joi.object({
+    title: Joi.string().min(2).max(100).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).max(100).required(),
+    dailyRentalRate: Joi.number().min(0).max(100).required(),
+  });
+  return schema.validate(movie);
+}
+module.exports.Movie = Movie;
+module.exports.validateMovie = validateMovie;
