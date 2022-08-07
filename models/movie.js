678d83660885ca6f0a99dc23f6570cc2fc402f4a
Ahim11 <ahim.mudbari@gmail.com>
2022-08-07 22:32:34 +0545

    schema and model for movie with joi validation and joi objectId refactoring

--- models/movie.js
+++ models/movie.js
@@ -32,7 +32,7 @@ const Movie = mongoose.model('Movie', movieSchema);
 function validateMovie(movie) {
   const schema = Joi.object({
     title: Joi.string().min(2).max(100).required(),
-    genreId: Joi.string().required(),
+    genreId: Joi.objectId().required(),
     numberInStock: Joi.number().min(0).max(100).required(),
     dailyRentalRate: Joi.number().min(0).max(100).required(),
   });
