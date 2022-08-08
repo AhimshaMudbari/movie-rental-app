2458d76351e1044458d82420dc8bd00747948970
Ahim11 <ahim.mudbari@gmail.com>
2022-08-08 22:10:11 +0545

    root of the project with new joi objectId and jwt

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const customer = require('./routes/customers');
 const movie = require('./routes/movies');
 const rental = require('./routes/rentals');
 const Joi = require('joi');
+const user = require('./routes/users');
+const login = require('./routes/login');
 Joi.objectId = require('joi-objectid')(Joi);
 
 require('dotenv').config();
 const port = process.env.PORT || 3500;
 const dbcon = process.env.MONGODB_CONNECTION;
+const jwtToken = process.env.JWTSECRET;
 
+if (!jwtToken) {
+  console.log('Fatal error: jwt secret key not defined');
+  process.exit(1);
+}
 mongoose
   .connect(dbcon)
   .then(() => {
@@ -26,6 +33,8 @@ app.use('/', home);
 app.use('/api/customers', customer);
 app.use('/api/movies', movie);
 app.use('/api/rentals', rental);
+app.use('/api/users', user);
+app.use('/api/login', login);
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
 });
