162a9641e0235f3563ed4a47e8f95ad27be9424a
Ahim11 <ahim.mudbari@gmail.com>
2022-08-09 22:07:15 +0545

    adding authorization middleware

--- routes/movies.js
+++ routes/movies.js
@@ -2,14 +2,14 @@ const express = require('express');
 const { Genre } = require('../models/genre');
 const router = express.Router();
 const { Movie, validateMovie } = require('../models/movie');
-const mongoose = require('mongoose');
+const auth = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
   const movies = await Movie.find().sort('title');
   res.send(movies);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validateMovie(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
   res.send(movie);
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', auth, async (req, res) => {
   const { error } = validateMovie(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -55,7 +55,7 @@ router.put('/:id', async (req, res) => {
   res.send(movie);
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
   const movie = await Movie.findByIdAndRemove(req.params.id);
   if (!movie) return res.status(404).send('movie not found.....');
   res.send(movie);
