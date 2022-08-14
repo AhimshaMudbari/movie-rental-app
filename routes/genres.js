1c6806954fbaa3f551e59629eae78e3e60fb1ab0
Ahim11 <ahim.mudbari@gmail.com>
2022-08-14 18:13:41 +0545

    adding async function to handle error and code refactoring

--- routes/genres.js
+++ routes/genres.js
@@ -3,21 +3,33 @@ const { Genre, validateGenre } = require('../models/genre');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
+// const asyncMiddleware = require('../middleware/async');
 
-router.get('/', async (req, res) => {
-  const genres = await Genre.find().sort('name');
-  res.send(genres);
-});
+router.get(
+  '/',
+  // asyncMiddleware(
+  async (req, res, next) => {
+    const genres = await Genre.find().sort('name');
+    res.send(genres);
+  }
+);
+// )
 
-router.post('/', auth, async (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const genre = new Genre({
-    name: req.body.name,
-  });
-  await genre.save();
-  res.send(genre);
-});
+router.post(
+  '/',
+  auth,
+  // asyncMiddleware(
+  async (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const genre = new Genre({
+      name: req.body.name,
+    });
+    await genre.save();
+    res.send(genre);
+  }
+);
+// )
 router.get('/:id', async (req, res) => {
   const genre = await Genre.findById(req.params.id);
   if (!genre) return res.status(404).send('genre of that ID not found');
