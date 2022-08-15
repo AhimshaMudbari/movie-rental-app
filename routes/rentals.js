7003de7ab6baa229969921c1adc3b8c898f73622
Ahim11 <ahim.mudbari@gmail.com>
2022-08-15 18:47:34 +0545

    adding asynchronous middleware function to catch error

--- routes/rentals.js
+++ routes/rentals.js
@@ -4,76 +4,92 @@ const { Movie } = require('../models/movie');
 const router = express.Router();
 const { Rental, validateRental } = require('../models/rental');
 const auth = require('../middleware/auth');
+const asyncMiddleware = require('../middleware/async');
+
 // const Fawn = require('fawn');
 // require('dotenv').config();
 
 // const con = process.env.MONGODB_FAWN;
 // Fawn.init(con);
 
-router.get('/', async (req, res) => {
-  const rental = await Rental.find().sort('-dateOut');
-  res.send(rental);
-});
+router.get(
+  '/',
+  asyncMiddleware(async (req, res) => {
+    const rental = await Rental.find().sort('-dateOut');
+    res.send(rental);
+  })
+);
 
-router.post('/', auth, async (req, res) => {
-  const { error } = validateRental(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  '/',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validateRental(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const customer = await Customer.findById(req.body.customerId);
-  if (!customer)
-    return res.status(404).send('Customer of the given ID not found');
+    const customer = await Customer.findById(req.body.customerId);
+    if (!customer)
+      return res.status(404).send('Customer of the given ID not found');
 
-  const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(404).send('Movie of the given ID not found');
+    const movie = await Movie.findById(req.body.movieId);
+    if (!movie) return res.status(404).send('Movie of the given ID not found');
 
-  if (movie.numberInStock === 0)
-    return res.status(400).send('there is no any movie in stock');
-  const rental = new Rental({
-    customer: {
-      _id: customer._id,
-      name: customer.name,
-      phone: customer.phone,
-    },
-    movie: {
-      _id: movie._id,
-      title: movie.title,
-      dailyRentalRate: movie.dailyRentalRate,
-    },
-  });
-  // try {
-  //   new Fawn.Task()
-  //     .save('rentals', rental)
-  //     .update(
-  //       'movies',
-  //       {
-  //         _id: movie._id,
-  //       },
-  //       {
-  //         $inc: {
-  //           numberInStock: -1,
-  //         },
-  //       }
-  //     )
-  //     .run();
-  //   res.send(rental);
-  // } catch (e) {
-  //   res.status(500).send('something went wrong ', e);
-  // }
-  await rental.save();
-  movie.numberInStock--;
-  movie.save();
-  res.send(rental);
-});
+    if (movie.numberInStock === 0)
+      return res.status(400).send('there is no any movie in stock');
+    const rental = new Rental({
+      customer: {
+        _id: customer._id,
+        name: customer.name,
+        phone: customer.phone,
+      },
+      movie: {
+        _id: movie._id,
+        title: movie.title,
+        dailyRentalRate: movie.dailyRentalRate,
+      },
+    });
+    // try {
+    //   new Fawn.Task()
+    //     .save('rentals', rental)
+    //     .update(
+    //       'movies',
+    //       {
+    //         _id: movie._id,
+    //       },
+    //       {
+    //         $inc: {
+    //           numberInStock: -1,
+    //         },
+    //       }
+    //     )
+    //     .run();
+    //   res.send(rental);
+    // } catch (e) {
+    //   res.status(500).send('something went wrong ', e);
+    // }
+    await rental.save();
+    movie.numberInStock--;
+    movie.save();
+    res.send(rental);
+  })
+);
 
-router.get('/:id', async (req, res) => {
-  const rental = await Rental.findById(req.params.id);
-  if (!rental) return res.status(404).send('Rental with given ID not found');
-  res.send(rental);
-});
+router.get(
+  '/:id',
+  asyncMiddleware(async (req, res) => {
+    const rental = await Rental.findById(req.params.id);
+    if (!rental) return res.status(404).send('Rental with given ID not found');
+    res.send(rental);
+  })
+);
 
-router.delete('/:id', auth, async (req, res) => {
-  const rental = await Rental.findByIdAndRemove(req.params.id);
-  if (!rental) return res.status(404).send('Invalid rental ID');
-  res.send(rental);
-});
+router.delete(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const rental = await Rental.findByIdAndRemove(req.params.id);
+    if (!rental) return res.status(404).send('Invalid rental ID');
+    res.send(rental);
+  })
+);
 module.exports = router;
