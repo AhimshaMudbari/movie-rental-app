fc82d9e398de76e6bb9d243003699a9ccf42c0b1
Ahim11 <ahim.mudbari@gmail.com>
2022-08-15 18:38:39 +0545

    logging error in file and db

--- middleware/error.js
+++ middleware/error.js
@@ -1,3 +1,8 @@
+const winston = require('winston');
+// const { createLogger, format } = require('winston');
+// const { prettyPrint, timestamp, colorize } = format;
+
 module.exports = function (err, req, res, next) {
+  winston.error(err.message, err);
   res.status(500).send('Something went wrong');
 };
