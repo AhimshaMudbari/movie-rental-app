04a46274a9c676308e96539e15ab1f7120bfd796
Ahim11 <ahim.mudbari@gmail.com>
2022-08-15 18:45:54 +0545

    adding asynchronous middleware function to catch error

--- routes/movies.js
+++ routes/movies.js
@@ -13,55 +13,69 @@ router.get(
   })
 );
 
-router.post('/', auth, async (req, res) => {
-  const { error } = validateMovie(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  '/',
+  asyncMiddleware(auth, async (req, res) => {
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genre.findById(req.body.genreId);
-  if (!genre) return res.status(404).send('genre not valid');
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(404).send('genre not valid');
 
-  const movie = new Movie({
-    title: req.body.title,
-    genre: {
-      _id: genre._id,
-      name: genre.name,
-    },
-    numberInStock: req.body.numberInStock,
-    dailyRentalRate: req.body.dailyRentalRate,
-  });
-  await movie.save();
-  res.send(movie);
-});
+    const movie = new Movie({
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    });
+    await movie.save();
+    res.send(movie);
+  })
+);
 
-router.get('/:id', async (req, res) => {
-  const movie = await Movie.findById(req.params.id);
-  if (!movie) return res.status(404).send('movie not found...');
-  res.send(movie);
-});
+router.get(
+  '/:id',
+  asyncMiddleware(async (req, res) => {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) return res.status(404).send('movie not found...');
+    res.send(movie);
+  })
+);
 
-router.put('/:id', auth, async (req, res) => {
-  const { error } = validateMovie(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-  const genre = await Genre.findById(req.body.genreId);
-  if (!genre) return res.status(404).send('invalid genre');
-  const movie = await Movie.findByIdAndUpdate(req.params.id, {
-    title: req.body.title,
-    genre: {
-      _id: genre._id,
-      name: genre.name,
-    },
-    numberInStock: req.body.numberInStock,
-    dailyRentalRate: req.body.dailyRentalRate,
-  });
-  if (!movie) return res.status(404).send('movie not found...');
-  movie.save();
-  res.send(movie);
-});
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(404).send('invalid genre');
+    const movie = await Movie.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      genre: {
+        _id: genre._id,
+        name: genre.name,
+      },
+      numberInStock: req.body.numberInStock,
+      dailyRentalRate: req.body.dailyRentalRate,
+    });
+    if (!movie) return res.status(404).send('movie not found...');
+    movie.save();
+    res.send(movie);
+  })
+);
 
-router.delete('/:id', auth, async (req, res) => {
-  const movie = await Movie.findByIdAndRemove(req.params.id);
-  if (!movie) return res.status(404).send('movie not found.....');
-  res.send(movie);
-});
+router.delete(
+  '/:id',
+  auth,
+  asyncMiddleware(async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+    if (!movie) return res.status(404).send('movie not found.....');
+    res.send(movie);
+  })
+);
 module.exports = router;
