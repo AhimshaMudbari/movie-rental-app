bfb7a410d03bc6f499bd7773d08e39d9ee7f3ba1
Ahim11 <ahim.mudbari@gmail.com>
2022-08-16 21:24:00 +0545

    created error log file for any error occuring in routes

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,26 @@
 const winston = require('winston');
-// const { createLogger, format } = require('winston');
-// const { prettyPrint, timestamp, colorize } = format;
+const { createLogger, format } = require('winston');
+const { prettyPrint, timestamp, colorize, combine } = format;
+require('winston-mongodb');
 
 module.exports = function (err, req, res, next) {
-  winston.error(err.message, err);
+  const logger = createLogger({
+    level: 'error',
+    format: combine(
+      prettyPrint(),
+      timestamp({
+        format: 'YYYY-MM-DD hh:mm:ss |',
+      }),
+      colorize()
+    ),
+    transports: [
+      new winston.transports.File({ filename: 'errorLog.log' }),
+      new winston.transports.MongoDB({
+        db: process.env.MONGODB_CONNECTION,
+        options: { useUnifiedTopology: true },
+      }),
+    ],
+  });
+  logger.error(err.message, err);
   res.status(500).send('Something went wrong');
 };
